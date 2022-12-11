47450e0806f9cb41dc5d94dea65dc0d733c315a1
Jamie McClymont <jamie@kwiius.com>
2022-12-11 17:39:02 +1300

    Fix rust build

--- default.nix
+++ default.nix
@@ -16,31 +16,5 @@ let
 in
 (mkRustPackageOrWorkspace
   {
-    # The root directory, which contains `Cargo.lock` and top-level `Cargo.toml`
-    # (the one containing `[workspace]` for workspace).
     src = (pkgs.nix-gitignore.gitignoreSource [ ] ./app);
-
-    # If you use registries other than crates.io, they should be imported in flake inputs,
-    # and specified here. Note that registry should be initialized via `mkIndex`,
-    # with an optional override.
-    #extraRegistries = {
-    # "https://example-registry.org" = nocargo.lib.${system}.mkIndex inputs.example-registry {};
-    #};
-
-    # If you use crates from git URLs, they should be imported in flake inputs,
-    # and specified here.
-    #gitSrcs = {
-    # "https://github.com/some/repo" = inputs.example-git-source;
-    #};
-
-    # If some crates in your dependency closure require packages from nixpkgs.
-    # You can override the argument for `stdenv.mkDerivation` to add them.
-    #
-    # Popular `-sys` crates overrides are maintained in `./crates-io-override/default.nix`
-    # to make them work out-of-box. PRs are welcome.
-    #buildCrateOverrides = with nixpkgs.legacyPackages.${system}; {};
-
-    # We use the rustc from nixpkgs by default.
-    # But you can override it, for example, with a nightly version from https://github.com/oxalica/rust-overlay
-    # rustc = rust-overlay.packages.${system}.rust-nightly_2022-07-01;
-  }).release
+  }).release.pinger.bin
