e71a2948836ae2ef40d8172995b66dba79ded712
Rhys Davies <rhys@memes.nz>
2022-12-13 21:20:26 +1300

    Tighten aws iam

--- faas/aws.py
+++ faas/aws.py
@@ -2,6 +2,9 @@ import pulumi
 import pulumi_aws as aws
 import pulumi_awsx as awsx
 from deps import nixdeps
+import json
+
+ACCOUNT_ID = "596309961293"
 
 
 class Deployer:
@@ -10,60 +13,69 @@ class Deployer:
         return aws.get_regions().names
 
     def __init__(self):
-        self.role = aws.iam.Role(
-            "mew",
-            assume_role_policy="""{
-          "Version": "2012-10-17",
-          "Statement": [
-            {
-              "Action": "sts:AssumeRole",
-              "Principal": {
-                "Service": "lambda.amazonaws.com"
-              },
-              "Effect": "Allow",
-              "Sid": ""
-            }
-          ]
-        }
-        """,
-        )
-        lambda_logging = aws.iam.Policy(
-            "lambdaLogging",
-            path="/",
-            description="IAM policy for logging from a lambda",
-            policy="""{
-          "Version": "2012-10-17",
-          "Statement": [
-            {
-              "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-              ],
-              "Resource": "arn:aws:logs:*:*:*",
-              "Effect": "Allow"
-            }
-          ]
-        }
-        """,
-        )
-        aws.iam.RolePolicyAttachment(
-            "lambdaLogs",
-            role=self.role.name,
-            policy_arn=lambda_logging.arn,
-        )
+        pass
 
     def make_function(self, location):
         provider = aws.Provider(f"aws-{location}", region=location)
         opts = pulumi.ResourceOptions(provider=provider)
 
+        role = aws.iam.Role(
+            f"pingerLambdaRole-{location}",
+            assume_role_policy=json.dumps(
+                {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Action": "sts:AssumeRole",
+                            "Principal": {"Service": "lambda.amazonaws.com"},
+                            "Effect": "Allow",
+                        }
+                    ],
+                }
+            ),
+        )
+
         lambda_ = aws.lambda_.Function(
-            f"awoo-{location}",
-            role=self.role.arn,
+            f"pinger-{location}",
+            role=role.arn,
             runtime="provided.al2",
             handler="thiscanbeanystring",
             code=pulumi.asset.FileArchive(nixdeps["aws.archive"]),
             opts=opts,
+        )
+
+        def lambda_policy(lambda_name: str) -> str:
+            return json.dumps(
+                {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Action": "logs:CreateLogGroup",
+                            "Resource": f"arn:aws:logs:{location}:{ACCOUNT_ID}:*",
+                        },
+                        {
+                            "Action": [
+                                "logs:CreateLogStream",
+                                "logs:PutLogEvents",
+                            ],
+                            "Resource": f"arn:aws:logs:{location}:{ACCOUNT_ID}:log-group:/aws/lambda/{lambda_name}:*",
+                            "Effect": "Allow",
+                        },
+                    ],
+                }
+            )
+
+        lambda_logging = aws.iam.Policy(
+            f"lambdaLogging-{location}",
+            path="/",
+            description="IAM policy for logging from a lambda",
+            policy=lambda_.name.apply(lambda_policy),
+        )
+        aws.iam.RolePolicyAttachment(
+            f"lambdaLogs-{location}",
+            role=role.name,
+            policy_arn=lambda_logging.arn,
         )
 
         url = aws.lambda_.FunctionUrl(
