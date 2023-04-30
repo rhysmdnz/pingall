159051c99b983610d251b753cac29156ea1efc1d
Rhys Davies <rhys@memes.nz>
2023-04-30 16:05:42 +1200

    Add service to ping all the things

---
+++ ping-service/ping_thing/main.py
@@ -0,0 +1,166 @@
+urls = {
+    "faas.alicloud": {
+        "ap-northeast-1": "https://pinger-ast-febd-fs-pingheast-cd-npmiyfwlgh.ap-northeast-1.fcapp.run",
+        "ap-southeast-1": "https://pinger-east-bfb-fs-pingheast-eb-bpqvsbldnh.ap-southeast-1.fcapp.run",
+        "ap-southeast-2": "https://pinger-heast-bd-fs-pingheast-dd-dmpccfqhvs.ap-southeast-2.fcapp.run",
+        "ap-southeast-3": "https://pinger-ast-aebf-fs-pingheast-ca-eeuzzgvdik.ap-southeast-3.fcapp.run",
+        "ap-southeast-5": "https://pinger-east-eff-fs-pingast-abdc-ceuybilhmn.ap-southeast-5.fcapp.run",
+        "cn-beijing": "https://pinger-eijing-f-fs-pingeijing-c-tvvyiqmeye.cn-beijing.fcapp.run",
+        "cn-chengdu": "https://pinger-engdu-ba-fs-pinghengdu-f-nxlamihcai.cn-chengdu.fcapp.run",
+        "cn-hangzhou": "https://pinger-ngzhou-f-fs-pinggzhou-cf-vvedculwry.cn-hangzhou.fcapp.run",
+        "cn-hongkong": "https://pinger-kong-eaf-fs-pingng-bffbf-nfkfvkcnpe.cn-hongkong.fcapp.run",
+        "cn-huhehaote": "https://pinger-ote-addd-fs-pinghaote-ef-bjjosejoce.cn-huhehaote.fcapp.run",
+        "cn-qingdao": "https://pinger-ngdao-af-fs-pingngdao-fc-jkvdquqpsp.cn-qingdao.fcapp.run",
+        "cn-shanghai": "https://pinger-ghai-ddb-fs-pinganghai-f-bonytmmndn.cn-shanghai.fcapp.run",
+        "cn-shenzhen": "https://pinger-nzhen-bc-fs-pingzhen-cdc-ksylcbiehk.cn-shenzhen.fcapp.run",
+        "cn-zhangjiakou": "https://pinger-jiakou-d-fs-pingakou-cbc-mgnclwsuni.cn-zhangjiakou.fcapp.run",
+        "eu-central-1": "https://pinger-entral-e-fs-pingentral-f-utzksrgzds.eu-central-1.fcapp.run",
+        "eu-west-1": "https://pinger-west-fb-fs-pingwest-adf-paalnnomeg.eu-west-1.fcapp.run",
+        "us-east-1": "https://pinger-ast-fcdf-fs-pingeast-dae-oqnnhkemlz.us-east-1.fcapp.run",
+        "us-west-1": "https://pinger-west-ec-fs-ping-west-eb-jjalrldaoi.us-west-1.fcapp.run",
+    },
+    "faas.aws": {
+        "ap-northeast-1": "https://dbu32ajwtrkf47h7dyirvmay5e0turxj.lambda-url.ap-northeast-1.on.aws/",
+        "ap-northeast-2": "https://igqr32yzc6ndipsohrb5ehl4ji0rmldn.lambda-url.ap-northeast-2.on.aws/",
+        "ap-northeast-3": "https://2w6lvr2bbdljjuknx7xoffka3a0zbezl.lambda-url.ap-northeast-3.on.aws/",
+        "ap-south-1": "https://yszs6w4dwekvqov2d3ufvprohe0nrmca.lambda-url.ap-south-1.on.aws/",
+        "ap-southeast-1": "https://behclkvr57vz4tbr5jthvuctje0elfbo.lambda-url.ap-southeast-1.on.aws/",
+        "ap-southeast-2": "https://iorbhr6uywvgfza2lw67ltixpi0ktzkl.lambda-url.ap-southeast-2.on.aws/",
+        "ca-central-1": "https://yh26xwra4dbvhirmzpdgpgj4tm0jpcfm.lambda-url.ca-central-1.on.aws/",
+        "eu-central-1": "https://uaxwxsnkpyudr7skgs6ktxf3ge0ekovw.lambda-url.eu-central-1.on.aws/",
+        "eu-north-1": "https://iqpumjtgcgwcnprpwaxg6l4ydi0vvicl.lambda-url.eu-north-1.on.aws/",
+        "eu-west-1": "https://jottclrkc7r73glotlryr3ciji0kybno.lambda-url.eu-west-1.on.aws/",
+        "eu-west-2": "https://kqgehpnxc7x7n5ebjbszgjxtpa0rtazj.lambda-url.eu-west-2.on.aws/",
+        "eu-west-3": "https://57opj5ekx33fzsesylfjoew67y0gfehi.lambda-url.eu-west-3.on.aws/",
+        "sa-east-1": "https://li5ahkxma53nrzwunvyavksujy0itrkz.lambda-url.sa-east-1.on.aws/",
+        "us-east-1": "https://xtwtv2jzxbx3x34b5vj4bb4zgi0femwt.lambda-url.us-east-1.on.aws/",
+        "us-east-2": "https://eyf722uurvoz244b26jjnqoy640mhzuz.lambda-url.us-east-2.on.aws/",
+        "us-west-1": "https://yt2k2h2txkqn4g4kqbgldqraiy0aglof.lambda-url.us-west-1.on.aws/",
+        "us-west-2": "https://avz4bicvqimipupt56h7ennxwe0vwrnj.lambda-url.us-west-2.on.aws/",
+    },
+    "faas.azure": {
+        "australiaeast": "https://ping-australiaeastb0aae614.azurewebsites.net/api/pinger",
+        "australiasoutheast": "https://ping-australiasoutheast40055e5c.azurewebsites.net/api/pinger",
+        "brazilsouth": "https://ping-brazilsouthf9ff0e3f.azurewebsites.net/api/pinger",
+        "canadacentral": "https://ping-canadacentral556acd8d.azurewebsites.net/api/pinger",
+        "canadaeast": "https://ping-canadaeastb9d5b173.azurewebsites.net/api/pinger",
+        "centralindia": "https://ping-centralindia524dfa4b.azurewebsites.net/api/pinger",
+        "centralus": "https://ping-centraluscdbb80df.azurewebsites.net/api/pinger",
+        "eastasia": "https://ping-eastasiad204df05.azurewebsites.net/api/pinger",
+        "eastus": "https://ping-eastuscfc71606.azurewebsites.net/api/pinger",
+        "eastus2": "https://ping-eastus2e8672878.azurewebsites.net/api/pinger",
+        "francecentral": "https://ping-francecentralc6c0b178.azurewebsites.net/api/pinger",
+        "japaneast": "https://ping-japaneast6c168000.azurewebsites.net/api/pinger",
+        "koreacentral": "https://ping-koreacentral834f0835.azurewebsites.net/api/pinger",
+        "northcentralus": "https://ping-northcentralus8004b28b.azurewebsites.net/api/pinger",
+        "northeurope": "https://ping-northeurope25f8842c.azurewebsites.net/api/pinger",
+        "norwayeast": "https://ping-norwayeast434e9b85.azurewebsites.net/api/pinger",
+        "southafricanorth": "https://ping-southafricanorth803cf74d.azurewebsites.net/api/pinger",
+        "southcentralus": "https://ping-southcentralus1e16bcb5.azurewebsites.net/api/pinger",
+        "southeastasia": "https://ping-southeastasia1ce3c51b.azurewebsites.net/api/pinger",
+        "swedencentral": "https://ping-swedencentral5adc913f.azurewebsites.net/api/pinger",
+        "switzerlandnorth": "https://ping-switzerlandnorthce4a155e.azurewebsites.net/api/pinger",
+        "uaenorth": "https://ping-uaenorth5c57ab34.azurewebsites.net/api/pinger",
+        "uksouth": "https://ping-uksouth4a618e2a.azurewebsites.net/api/pinger",
+        "ukwest": "https://ping-ukwestb7f38144.azurewebsites.net/api/pinger",
+        "westcentralus": "https://ping-westcentralus4d7676d3.azurewebsites.net/api/pinger",
+        "westeurope": "https://ping-westeuropebe6219e9.azurewebsites.net/api/pinger",
+        "westindia": "https://ping-westindiabf1f1ef0.azurewebsites.net/api/pinger",
+        "westus": "https://ping-westus5699b640.azurewebsites.net/api/pinger",
+        "westus3": "https://ping-westus3e9ba5355.azurewebsites.net/api/pinger",
+    },
+    "faas.gcp": {
+        "asia-east1": "https://ping-asia-east1-tx45f4bnfa-de.a.run.app",
+        "asia-east2": "https://ping-asia-east2-tx45f4bnfa-df.a.run.app",
+        "asia-northeast1": "https://ping-asia-northeast1-tx45f4bnfa-an.a.run.app",
+        "asia-northeast2": "https://ping-asia-northeast2-tx45f4bnfa-dt.a.run.app",
+        "asia-northeast3": "https://ping-asia-northeast3-tx45f4bnfa-du.a.run.app",
+        "asia-south1": "https://ping-asia-south1-tx45f4bnfa-el.a.run.app",
+        "asia-south2": "https://ping-asia-south2-tx45f4bnfa-em.a.run.app",
+        "asia-southeast1": "https://ping-asia-southeast1-tx45f4bnfa-as.a.run.app",
+        "asia-southeast2": "https://ping-asia-southeast2-tx45f4bnfa-et.a.run.app",
+        "australia-southeast1": "https://ping-australia-southeast1-tx45f4bnfa-ts.a.run.app",
+        "australia-southeast2": "https://ping-australia-southeast2-tx45f4bnfa-km.a.run.app",
+        "europe-central2": "https://ping-europe-central2-tx45f4bnfa-lm.a.run.app",
+        "europe-north1": "https://ping-europe-north1-tx45f4bnfa-lz.a.run.app",
+        "europe-southwest1": "https://ping-europe-southwest1-tx45f4bnfa-no.a.run.app",
+        "europe-west1": "https://ping-europe-west1-tx45f4bnfa-ew.a.run.app",
+        "europe-west12": "https://ping-europe-west12-tx45f4bnfa-og.a.run.app",
+        "europe-west2": "https://ping-europe-west2-tx45f4bnfa-nw.a.run.app",
+        "europe-west3": "https://ping-europe-west3-tx45f4bnfa-ey.a.run.app",
+        "europe-west4": "https://ping-europe-west4-tx45f4bnfa-ez.a.run.app",
+        "europe-west6": "https://ping-europe-west6-tx45f4bnfa-oa.a.run.app",
+        "europe-west8": "https://ping-europe-west8-tx45f4bnfa-oc.a.run.app",
+        "europe-west9": "https://ping-europe-west9-tx45f4bnfa-od.a.run.app",
+        "me-central1": "https://ping-me-central1-tx45f4bnfa-ww.a.run.app",
+        "me-west1": "https://ping-me-west1-tx45f4bnfa-zf.a.run.app",
+        "northamerica-northeast1": "https://ping-northamerica-northeast1-tx45f4bnfa-nn.a.run.app",
+        "northamerica-northeast2": "https://ping-northamerica-northeast2-tx45f4bnfa-pd.a.run.app",
+        "southamerica-east1": "https://ping-southamerica-east1-tx45f4bnfa-rj.a.run.app",
+        "southamerica-west1": "https://ping-southamerica-west1-tx45f4bnfa-tl.a.run.app",
+        "us-central1": "https://ping-us-central1-tx45f4bnfa-uc.a.run.app",
+        "us-east1": "https://ping-us-east1-tx45f4bnfa-ue.a.run.app",
+        "us-east4": "https://ping-us-east4-tx45f4bnfa-uk.a.run.app",
+        "us-east5": "https://ping-us-east5-tx45f4bnfa-ul.a.run.app",
+        "us-south1": "https://ping-us-south1-tx45f4bnfa-vp.a.run.app",
+        "us-west1": "https://ping-us-west1-tx45f4bnfa-uw.a.run.app",
+        "us-west2": "https://ping-us-west2-tx45f4bnfa-wl.a.run.app",
+        "us-west3": "https://ping-us-west3-tx45f4bnfa-wm.a.run.app",
+        "us-west4": "https://ping-us-west4-tx45f4bnfa-wn.a.run.app",
+    },
+}
+
+
+from fastapi import FastAPI
+import aiohttp
+import asyncio
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root(url):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        results = {}
+        for provider, regions in urls.items():
+            results[provider] = {}
+            for region, uurl in regions.items():
+                task = asyncio.create_task(session.get(f"{uurl}?url={url}"))
+                print(task)
+                tasks.append(task)
+                results[provider][region] = task
+        await asyncio.gather(*tasks)
+        results_list = []
+        errors = []
+        for provider, regions in results.items():
+            for region, task in regions.items():
+                response = task.result()
+                if response.ok:
+                    results_list.append(
+                        {
+                            "region": f"{provider}.{region}",
+                            "time": await response.text(),
+                        }
+                    )
+                errors.append(
+                    {
+                        "region": f"{provider}.{region}",
+                        "status_code": response.status,
+                        "body": await response.text(),
+                    }
+                )
+    results_list.sort(key=lambda a: float(a["time"]))
+    # results_list.sort(key=lambda a: a["time"])
+    return {"results": results_list, "errors": errors}
+
+
+@app.get("/liveness_check")
+async def liveness_check():
+    return "Ok!"
+
+
+@app.get("/readiness_check")
+async def readiness_check():
+    return "Ok!"
