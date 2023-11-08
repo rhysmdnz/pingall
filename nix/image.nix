619a6380a95a9290e1606f840dfcc1b68677c073
Jamie McClymont <jamie@kwiius.com>
2023-11-08 19:56:11 +1100

    uncompressed images

--- nix/image.nix
+++ nix/image.nix
@@ -1,4 +1,16 @@
-{ lib, dockerTools, pinger, name }: dockerTools.buildLayeredImage {
+{ lib, runCommand, dockerTools, pinger, name }: 
+let buildUncompressedLayeredImage = lib.makeOverridable ({ name, ... }@args:
+    let
+      stream = dockerTools.streamLayeredImage args;
+    in
+    runCommand "${baseNameOf name}.tar"
+      {
+        inherit (stream) imageName;
+        passthru = { inherit (stream) imageTag; };
+      } "${stream} > $out"
+  );
+in
+buildUncompressedLayeredImage {
   # namespaced to a domain we control, because otherwise dependency confusion on docker hub becomes a concern
   name = name;
   # version the image with the hash of the pinger we're using.
